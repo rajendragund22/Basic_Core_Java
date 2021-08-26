524cb2b4fe8c5aefcc434ed6ca9029e80d07ef1f
RajendraGund <rajendragund2015@gmail.com>
2021-08-26 21:22:40 +0530

    Program for find factor

---
+++ Factor.java
@@ -0,0 +1,20 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class Factor {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
