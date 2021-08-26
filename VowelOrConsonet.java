92d77f0ab9c1fbc6b3b8ceef08b3ea6c70cc5049
RajendraGund <rajendragund2015@gmail.com>
2021-08-26 21:55:34 +0530

    Program for find Vowels and Consonants using if else

---
+++ VowelOrConsonet.java
@@ -0,0 +1,14 @@
+package basicPrograms;
+
+public class VowelOrConsonet {
+	public static void main(String[] args) {
+		char alfa = 'a';
+		if (alfa == 'a' || alfa == 'e' || alfa == 'o' || alfa == 'i' || alfa == 'u') {
+			System.out.println("This is a Vowel");
+		} else {
+			System.out.println("this is Consonant");
+		}
+
+	}
+
+}
