e7ba7fce22d727dc08c34c42cac7eeb775f270aa
RajendraGund <rajendragund2015@gmail.com>
2021-08-26 21:41:03 +0530

    Program for find the power of two

---
+++ PowerOf2.java
@@ -0,0 +1,17 @@
+package basicPrograms;
+import java.util.Scanner;
+public class PowerOf2 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the power of 2 : ");
+		Scanner sc = new Scanner(System.in);
+		int num = 2;
+		int power;
+		power = sc.nextInt();
+		sc.close();
+
+		System.out.println(Math.pow(num, power));
+
+	}
+
+}
